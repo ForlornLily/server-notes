1ecae0081ad2abd8befeffefc3ad21c468156f95
Forlorn <tudoutasty@gmail.com>
2020-11-05 19:07:58 +0800

    fix: gh-pages url

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f "https://${access_token}@github.com/ForlornLily/server-notes.git" main:gh-pages
+git push -f "https://${access_token}@github.com/ForlornLily/server-notes.git" master:gh-pages
 
 cd -
